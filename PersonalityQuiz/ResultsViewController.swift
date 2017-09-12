5c4cc4b69561950c519abe4f441a1e39f82b3e38
yewei600 <ericwei94@gmail.com>
2017-09-12 06:29:31 -0400

    Initial Commit

---
+++ PersonalityQuiz/ResultsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ResultsViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Eric Wei on 2017-09-12.
+//  Copyright © 2017 EricWei. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class ResultsViewController: UIViewController {
+    
+    
+}
